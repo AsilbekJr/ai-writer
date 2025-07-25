2996af05858c168575c0713ee0a4223e229b2014
Asilbek <jaoripovich@gmail.com>
2025-07-25 09:10:51 +0500

    git init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl underline ">Hello</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button variant="destructive">Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
