147095ca9144e625a4217f995a12ac20d9461c90
Asilbek <jaoripovich@gmail.com>
2025-07-30 09:30:54 +0500

    feat/42: Gemini AI response component created

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -1,6 +1,6 @@
-import { Pencil } from 'lucide-react';
 import PromptHistory from './prompt-history';
 import type { TPromptHistory } from '@/shared/types/propmt-history.type';
+import { PencilSquareIcon } from '@heroicons/react/24/solid';
 
 const mockItems: TPromptHistory[] = [
   {
@@ -37,7 +37,7 @@ export default function Sidebar() {
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold">AI Writer</h1>
         <button>
-          <Pencil size={24} />
+          <PencilSquareIcon className="w-6" />
         </button>
       </div>
       <PromptHistory items={mockItems} />
