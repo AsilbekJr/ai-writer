a5fc39bfab90483717b290b922aaa4d8a2f554ab
Asilbek <jaoripovich@gmail.com>
2025-08-06 18:41:52 +0500

    feat50:feedback yig'ish

--- src/pages/dashboard-content.tsx
+++ src/pages/dashboard-content.tsx
@@ -1,6 +1,8 @@
 import ContentViewer from '@/components/dashboard/content-viewer';
 import { useContentContext } from '@/contexts/content.context';
 import type { TGeneratedContent } from '@/shared/types/generating-content';
+import { StarIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -25,9 +27,35 @@ export default function DashboardContent() {
     return <h3>Not Found</h3>;
   }
 
+  const handleChangeRate = (rate: number) => {
+    if (generatedContent) {
+      handleSave({
+        ...generatedContent,
+        rate,
+      });
+    }
+  };
+
   return (
     <div>
-      <h1 className="text-3xl font-semibold my-3">{generatedContent.title}</h1>
+      <div className="flex gap-1">
+        <h1 className="text-3xl font-semibold my-3">
+          {generatedContent.title}
+        </h1>
+        <div className="flex gap-1 items-center">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <StarIcon
+                onClick={() => handleChangeRate(index + 1)}
+                className={clsx(
+                  'w-7 h-7 cursor-pointer',
+                  (generatedContent.rate || 0) > index && 'fill-black'
+                )}
+              />
+            ))}
+        </div>
+      </div>
       <ContentViewer
         generatedContent={generatedContent}
         key={generatedContent.id}
