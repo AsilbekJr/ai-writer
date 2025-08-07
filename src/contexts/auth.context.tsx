beb572fe2889bf63b3789c1f03415246d98dae90
Asilbek <jaoripovich@gmail.com>
2025-08-07 10:39:17 +0500

    feat/53 user login created

--- src/contexts/auth.context.tsx
+++ src/contexts/auth.context.tsx
@@ -3,7 +3,8 @@ import { createContext, useContext, type ReactNode, type FC } from 'react';
 import { useLocalStorage } from 'react-use';
 
 interface IAuthContext {
-  register: (login: string, password: string) => void;
+  registerUser: (login: string, password: string) => void;
+  loginUser: (login: string, password: string) => TRegisteredUser;
 }
 
 export const AuthContext = createContext<IAuthContext | null>(null);
@@ -24,14 +25,28 @@ interface IProps {
 
 const AuthProvider: FC<IProps> = ({ children }) => {
   const [users, setUsers] = useLocalStorage<TRegisteredUser[]>('users', []);
-  const register = (login: string, password: string) => {
+  const registerUser = (login: string, password: string) => {
     if (users) {
       setUsers([...users, { login, password, createdAt: new Date() }]);
     }
   };
 
+  const loginUser = (login: string, password: string) => {
+    const user = users?.find((u) => u.login === login);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+    if (user.password !== password) {
+      throw new Error('Invalid Password');
+    }
+    return user;
+  };
+
   return (
-    <AuthContext.Provider value={{ register }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ registerUser, loginUser }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
